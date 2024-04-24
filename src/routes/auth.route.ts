ee9adfd4dbc24323b079e4e56621ab9b13e1768a
hiepnn-1339 <nguyen.ngoc.hiep@sun-asterisk.com>
2024-04-24 09:44:32 +0700

    user_logout

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,5 +4,6 @@ import * as authController from '../controllers/auth.controller';
 
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
+router.get('/logout', authController.getLogout);
 
 export default router;
