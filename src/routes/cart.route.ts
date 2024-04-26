64b82d5d43d0b098e3ce612f87b573ddafa37910
hiepnn-1339 <nguyen.ngoc.hiep@sun-asterisk.com>
2024-04-27 01:47:56 +0700

    user_delete_cart_item

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 import * as cartController from '../controllers/cart.controller';
 
+router.delete('/:id', cartController.deleteCartItem);
 router.post('/update', cartController.postAddItemToCart);
 router.get('/', cartController.getCart);
 
